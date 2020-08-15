1b65297f67b45c6aa61d7baf0522149ea476c046
frankwaldal <frank.sondre.waldal@entur.org>
2020-08-15 02:32:48 +0200

    Finish basic styling

--- src/Site/Components/Header.jsx
+++ src/Site/Components/Header.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const toggleMobileMenu = (e) => {
+    if (e.currentTarget.className === 'mobileButton') {
+      setMobileOpen(!mobileOpen);
+    } else {
+      setMobileOpen(false);
+    }
+  }
   return (
     <header>
       <h1>fwaldal.no</h1>
-      <nav>
+      <nav className={mobileOpen ? 'mobileOpen' : 'mobileClosed'}>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/' onClick={toggleMobileMenu}>Home</Link>
           </li>
+          <div className='menuDivider' />
           <li>
-            <Link to='/about'>About me</Link>
+            <Link to='/about' onClick={toggleMobileMenu}>About me</Link>
           </li>
+          <div className='menuDivider' />
           <li>
-            <Link to='/work'>My projects</Link>
+            <Link to='/work' onClick={toggleMobileMenu}>My projects</Link>
           </li>
+          <div className='menuDivider' />
           <li>
-            <Link to='/experience'>Work and Education</Link>
+            <Link to='/experience' onClick={toggleMobileMenu}>Work and Education</Link>
           </li>
         </ul>
       </nav>
+      <div className='mobileButton' onClick={toggleMobileMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
   )
 }
