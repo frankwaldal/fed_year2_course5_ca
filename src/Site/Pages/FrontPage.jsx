b8d3b8db34902a767b0924dc061892b346e64833
frankwaldal <frank.sondre.waldal@entur.org>
2020-08-16 05:09:38 +0200

    More content added

--- src/Site/Pages/FrontPage.jsx
+++ src/Site/Pages/FrontPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import codeImage from '../../Assets/codeScreenshot.JPG';
+
 export default function FrontPage() {
   return (
-    <main>
-      <h1>Front Page</h1>
+    <main className='frontPage'>
+      <div style={{gridColumn: 'span 2', height: '20px'}} />
+      <div>
+        
+      </div>
+      <img src={codeImage} alt='Screenshot from codesnippet' />
     </main>
   )
 }
